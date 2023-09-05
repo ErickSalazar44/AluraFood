29f2e5df1be0c30f00d0018ffeceb6012b8796dd
ErickSalazar44 <erickearl22@gmail.com>
2023-09-05 14:04:05 -0500

    'correccion en la validacion telefono'

--- src/Form/DatosPersonales/validaciones.js
+++ src/Form/DatosPersonales/validaciones.js
@@ -8,5 +8,5 @@ export const validarTelefono = (number) => {
     const validar = /^[09][0-9]{1,7}$/;
     const longitud = number.toString().length;
 
-    return !validar.test(number) && longitud > 3 ? true : false;
+    return !validar.test(number) && longitud > 8 ? true : false;
 };
